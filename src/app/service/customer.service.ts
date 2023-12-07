e562a5a702e32e26612c0fef452611bf9a59403d
cherryApp <j.cserko@gmail.com>
2023-12-07 18:07:40 +0100

    starting state

--- src/app/service/customer.service.ts
+++ src/app/service/customer.service.ts
@@ -35,8 +35,4 @@ export class CustomerService {
     return this.http.delete<Customer>(`${this.apiUrl}${customer.id}`);
   }
 
-  query(query: string): Observable<Customer[]> {
-    return this.http.get<Customer[]>(`${this.apiUrl}?${query}`);
-  }
-
 }
--- src/app/store/CustomerStore.ts
+++ src/app/store/CustomerStore.ts
@@ -44,12 +44,6 @@ export class CustomerStore {
     }
   }
 
-  async createItem(customer: Customer) {
-    const created = await firstValueFrom( this.customerService.create(customer) );
-    const list = [...this.state.list(), created];
-    patchState(this.state, { list });
-  }
-
   async load() {
     if (this.list().length > 0) return;
 
